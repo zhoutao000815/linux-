4b1c5ba80149405bec54540448173d5c7767a889
zhoutao <1761885173@qq.com>
2021-02-07 17:12:47 +0800

    僵尸进程模拟

---
+++ Ztest.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<unistd.h>
+int main()
+{
+   pid_t id = fork();
+   if(id < 0){
+      perror("fork");
+       return 1;
+   }
+   else if(id > 0){ //parent
+  printf("parent[%d] is sleeping...\n", getpid());
+    sleep(30);
+   }else{
+      printf("child[%d] is begin Z...\n", getpid());
+       sleep(5);
+        exit(EXIT_SUCCESS);
+   }
+    return 0;
+}
